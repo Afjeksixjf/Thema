cd61a32a94ecba9a2b7d1bfc7016d921b696201b
BarModssOfficial <142868847+Afjeksixjf@users.noreply.github.com>
2024-11-16 11:31:40 +0700

    Update install.sh

--- install.sh
+++ install.sh
@@ -56,7 +56,7 @@ install_jq() {
 check_token() {
   echo -e "                                                       "
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
-  echo -e "${BLUE}[+]               LICENSY REVXZ OFFC                [+]${NC}"
+  echo -e "${BLUE}[+]               LICENSY BAR MODS                  [+]${NC}"
   echo -e "${BLUE}[+] =============================================== [+]${NC}"
   echo -e "                                                       "
   echo -e "${YELLOW}MASUKAN AKSES TOKEN :${NC}"
